5102c0a8fd8263af921212dd91b5d8bb80e5b600
Gonzalo <gonzadevelw@gmail.com>
2022-10-07 12:56:29 -0500

    agregada url img

--- routes/Products.js
+++ routes/Products.js
@@ -3,6 +3,37 @@ import Product from '../schemas/Product.js'
 
 const productRouter = Router()
 
+productRouter.get('/products', (req, res, next) => {
+  const host = req.protocol + '://' + req.get('host')
+
+  Product.find({}).then(products => {
+    products.forEach(p => {
+      const { id, category, description, nameProduct } = p
+      Product.findByIdAndUpdate(
+        id,
+        {
+          $set: {
+            imgURL:
+              host +
+              '/api/images/' +
+              category.replace(/ /g, '') +
+              '/' +
+              nameProduct.replace(/ /g, '') +
+              '-' +
+              description.replace(/ /g, '').replace('"', 'p') +
+              '.webp',
+          },
+        },
+        { new: true }
+      )
+        .then()
+        .catch(e => console.error(e))
+    })
+  })
+
+  next()
+})
+
 productRouter.get('/products', (req, res) => {
   Product.find({}).then(products => {
     res.json(products)
