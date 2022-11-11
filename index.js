d15e3597ecac2a4667f5743f0ceb7efebcf49271
Gonzalo <gonzadevelw@gmail.com>
2022-11-11 09:50:47 -0500

    add routes users, not hash password

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import express from 'express'
-import cnxMongo from './config/cnxMongo.js'
-import cors from 'cors'
-import productRouter from './routes/Products.js'
-import dotennv from 'dotenv'
-import imageRouter from './routes/Image.js'
-import RootRouter from './routes/Root.js'
-import compression from 'compression'
-const app = express()
+import express from "express";
+import cnxMongo from "./config/cnxMongo.js";
+import cors from "cors";
+import productRouter from "./routes/Products.js";
+import dotennv from "dotenv";
+import imageRouter from "./routes/Image.js";
+import RootRouter from "./routes/Root.js";
+import compression from "compression";
+import userRoter from "./routes/Users.js";
 
-dotennv.config()
-app.use(compression())
-app.use(cors())
-app.use(express.json()) // parsea el req.body de las peticiones post
+const app = express();
 
-app.use(RootRouter)
-app.use('/api', productRouter)
-app.use('/api', imageRouter)
+dotennv.config();
+app.use(compression());
+app.use(cors());
+app.use(express.json()); // parsea el req.body de las peticiones post
 
-const PORT = process.env.PORT || 4050
+app.use(RootRouter);
+app.use("/api", productRouter);
+app.use("/api", imageRouter);
+app.use("/api", userRoter);
+
+const PORT = process.env.PORT || 4050;
 app.listen(PORT, () => {
-  cnxMongo()
-})
+  cnxMongo();
+});
