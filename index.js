2c71c938486201af2bbf3a41d6f15c73920e4558
Gonzalo <gonzadevelw@gmail.com>
2022-12-02 08:52:56 -0500

    replace route images for  app.use static

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cnxMongo from "./config/cnxMongo.js";
 import cors from "cors";
 import productRouter from "./routes/Products.js";
 import dotennv from "dotenv";
-import imageRouter from "./routes/Image.js";
 import RootRouter from "./routes/Root.js";
 import compression from "compression";
 import userRoter from "./routes/Users.js";
@@ -11,13 +10,13 @@ import userRoter from "./routes/Users.js";
 const app = express();
 
 dotennv.config();
+app.use('/api/images', express.static('public/Images'))
 app.use(compression());
 app.use(cors());
 app.use(express.json()); // parsea el req.body de las peticiones post
 
 app.use(RootRouter);
 app.use("/api", productRouter);
-app.use("/api", imageRouter);
 app.use("/api", userRoter);
 
 const PORT = process.env.PORT || 4050;
